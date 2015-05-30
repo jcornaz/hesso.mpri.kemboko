d97bd6f7baadc2fd2101204d9cee32393f057a26
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-30 16:15:42 +0200

    New ANN with a new feature of the hand to improve the resulting quality

--- src/feature/features_to_extract.m
+++ src/feature/features_to_extract.m
@@ -5,7 +5,7 @@ function [flist] = features_to_extract()
     shoulder = []; %[6,8,11,15];
     elbow = [1:3,5,6]; %[1,4,6,11,12,17];
     wrist = [1:6,8,9,14,17]; %[1,3,4,5,6,8,9,10,11,12,14,17];
-    palm = [1:10,14,17];   %[3,5,7,8,9,10,11,12,14,15,17];
+    palm = [1:10,14,15,17];   %[3,5,7,8,9,10,11,12,14,15,17];
 
     xsens = [shoulder,elbow + 17,wrist + 2*17,palm + 3*17]';
     xsens = [ones(length(xsens),1),xsens];
