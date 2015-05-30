c2dbd50325aaec7b5fbdd794cf0f659964ca7242
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-30 09:05:20 +0200

    ANN trained with a better result

--- src/ann/run.m
+++ src/ann/run.m
@@ -1,23 +1,14 @@
-% Accelerometers features to use
-shoulder = [6,8,11,15];
-elbow = [1,4,6,11,12,17];
-wrist = [1,3,4,5,6,8,9,10,11,12,14,17];
-palm = [3,5,7,8,9,10,11,12,14,15,17];
-
-xsens = [shoulder,elbow + 17,wrist + 2*17,palm + 3*17]';
-xsens = [ones(length(xsens),1),xsens];
-
-% Kinect features to use
-kinect = [37;41;45;46];
-kinect = [ones(length(kinect),1)*2,kinect];
-
-% Features list to use
-flist = [xsens;kinect];
-
-% Load data set
-dataset = load('./data/Dataset_segmented.mat');
-occurrences = dataset.Dataset.Data.occurence;
-
-% Prepare dataset
-[inputs, outputs] = prepare_dataset(occurrences,flist,@fourierTransform);
-outputs = to_bool_matrix(outputs);
+function [predicted] = run( testset_path )
+    FeaturesToExtract = [1:9 18:29];
+    %load('myModel.mat', 'model'); this dummy example does not use a model
+    model = [];
+    testSet = LoadTestSet(testset_path,FeaturesToExtract); %in this function, when the dataset is loaded, the labels are converted from 0 to 10 to 1 to 11 for matlab
+    predicted = Recognize(model, testSet);
+    predicted = int32(predicted)-1; %this is very important (in the original dataset, classes are labelled from 0 to 10 and not from 1 to 11)
+    filename = 'results.txt';
+    fid = fopen(filename, 'w');
+    dlmwrite(filename,predicted,'-append',... 
+     'delimiter',' ',...
+     'newline','pc');
+    fclose(fid);
+end
