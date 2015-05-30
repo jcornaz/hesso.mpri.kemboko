0c0dbe5695045fc0b8cd1ed546b68acc7e204113
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-30 11:36:08 +0200

    Split dataset implemented but not working

--- src/ann/prepare_train_dataset.m
+++
@@ -1,7 +0,0 @@
-% Load data set
-dataset = load('./data/Dataset_segmented.mat');
-occurrences = dataset.Dataset.Data.occurence;
-
-% Prepare dataset
-[inputs, outputs] = prepare_dataset(occurrences,features_to_extract(),true,@fourierTransform);
-outputs = to_bool_matrix(outputs);
