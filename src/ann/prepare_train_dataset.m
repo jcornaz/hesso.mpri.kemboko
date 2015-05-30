64f00fa9cedbbcae7e49ced08a43e81dd8dc9338
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-30 11:07:08 +0200

    New ANN trained with better result (F1 score = 0.9706 with trainset as testset)

--- src/ann/prepare_train_dataset.m
+++ src/ann/prepare_train_dataset.m
@@ -3,5 +3,5 @@ dataset = load('./data/Dataset_segmented.mat');
 occurrences = dataset.Dataset.Data.occurence;
 
 % Prepare dataset
-[inputs, outputs] = prepare_dataset(occurrences,true,features_to_extract(),@fourierTransform);
+[inputs, outputs] = prepare_dataset(occurrences,features_to_extract(),true,@fourierTransform);
 outputs = to_bool_matrix(outputs);
