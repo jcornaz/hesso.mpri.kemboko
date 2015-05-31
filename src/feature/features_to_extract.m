a5485f05e661a15d2470e929f637ad88e161a0fc
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-31 22:41:04 +0200

    New ANN with less features

--- src/feature/features_to_extract.m
+++ src/feature/features_to_extract.m
@@ -3,9 +3,9 @@ function [flist] = features_to_extract()
 
     % Accelerometers features to use with standard deviation
     shoulder = [];
-    elbow = 1:4;
-    wrist = [3,4,8,9,12,14,17];
-    palm = [5,7,8:12,14,15,17];
+    elbow = [1,4];
+    wrist = [4,8,9,12,14,17];
+    palm = [5,8:10,14,15,17];
 
     xsens_std = [shoulder,elbow + 17,wrist + 2*17,palm + 3*17];
     xsens_std = [ones(length(xsens_std),1),xsens_std',zeros(length(xsens_std),1)];
@@ -13,8 +13,8 @@ function [flist] = features_to_extract()
     % Accelerometers features to use with mean
     shoulder = [];
     elbow = 1:3;
-    wrist = [8,12,14,15,17];
-    palm = [8,9,11,12,14,15,17];
+    wrist = [8,12,14,17];
+    palm = [8,12,14,17];
 
     xsens_mean = [shoulder,elbow + 17,wrist + 2*17,palm + 3*17];
     xsens_mean = [ones(length(xsens_mean),1),xsens_mean',ones(length(xsens_mean),1)];
