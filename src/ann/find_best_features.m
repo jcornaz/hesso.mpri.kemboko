548cd8eb646c319370d13cbb99772bef2a3ed63a
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-31 07:57:09 +0200

    find best feature function improved

--- src/ann/find_best_features.m
+++ src/ann/find_best_features.m
@@ -1,23 +1,11 @@
 function [best_features,best_scores] = find_best_features( occurences )
-    NB_TRY = 5;
+    NB_TRY = 6;
     scores = zeros([1,NB_TRY]);
     
     % Accelerometers features to use
-    xsenses = [1:12, 14:17];
-
-    xsens = [xsenses + 17,xsenses + 2*17,xsenses + 3*17]';
-    xsens = [ones(length(xsens),1),xsens];
-
-    % Kinect features to use
-    kinect = [37:39,41:43,45:46]';
-    kinect = [ones(length(kinect),1)*2,kinect];
-
-    % Features list to use
-    flist = [xsens;kinect];
+    best_features = minfeatures();
+    flist = optfeatures();
     
-    first_feature_id = numel(xsenses) * 3 + 8;
-    best_features = flist(first_feature_id,:);
-    flist(first_feature_id,:) = [];
     best_scores = [];
     
     global_best_score = -inf;
@@ -30,7 +18,12 @@ function [best_features,best_scores] = find_best_features( occurences )
             tmpflist = [best_features;flist(i,:)];
             for j = 1:NB_TRY
                 [train_inputs, train_outputs, test_inputs, test_outputs] = prepare_traintest_dataset(occurences, 0.2, tmpflist);
-                [net,~] = train(feedforwardnet(10),train_inputs', to_bool_matrix(train_outputs)');
+                net = feedforwardnet(10);
+                net.trainParam.max_fail = 10;
+                net.divideParam.trainRatio = 0.8;
+                net.divideParam.valRatio = 0.2;
+                net.divideParam.testRatio = 0;
+                [net,~] = train(net, train_inputs', to_bool_matrix(train_outputs)');
                 scores(j) = f1_score( test_outputs, to_labelid_vector( net( test_inputs' )' ) );
             end
             score = mean( scores );
@@ -54,4 +47,38 @@ function [best_features,best_scores] = find_best_features( occurences )
         
         disp( best_features );
     end
+end
+
+function [features] = minfeatures()
+    shoulder = [];
+    elbow = [1,4];
+    wrist = [8,14,17];
+    palm = [8:10,14,15];
+
+    xsens = [shoulder,elbow + 17,wrist + 2*17,palm + 3*17]';
+    xsens = [ones(length(xsens),1),xsens];
+
+    % Kinect features to use
+    kinect = [];
+    kinect = [ones(length(kinect),1)*2,kinect];
+
+    % Features list to use
+    features = [xsens;kinect];
+end
+
+function [features] = optfeatures()
+    shoulder = [1,2,6,11];
+    elbow = [2,3,5,6,8,12];
+    wrist = [1:6,9:12,15,16];
+    palm = [1:7,11,12,17];
+
+    xsens = [shoulder,elbow + 17,wrist + 2*17,palm + 3*17]';
+    xsens = [ones(length(xsens),1),xsens];
+
+    % Kinect features to use
+    kinect = []; 
+    kinect = [ones(length(kinect),1)*2,kinect];
+
+    % Features list to use1
+    features = [xsens;kinect];
 end
