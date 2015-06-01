30aea564564cdac38673bb7a0a154d10d702fb28
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-06-01 10:37:27 +0200

    Last ANN

--- src/feature/features_to_extract.m
+++ src/feature/features_to_extract.m
@@ -2,11 +2,12 @@ function [flist] = features_to_extract()
 % Return the list of the feature to use
 
     % Accelerometers features to use with standard deviation
-    shoulder = [];
-    elbow = [1,4];
-    wrist = [4,8,9,12,14,17];
-    palm = [5,8:10,14,15,17];
+    shoulder = 1;
+    elbow = [1,2,4];
+    wrist = [2,4,8,10,14,16,17];
+    palm = [2,4,8:10,12,14,15];
 
+    
     xsens_std = [shoulder,elbow + 17,wrist + 2*17,palm + 3*17];
     xsens_std = [ones(length(xsens_std),1),xsens_std',zeros(length(xsens_std),1)];
     
